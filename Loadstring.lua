aabf602c6aac3aab291274bfdfc85068847dcad0
kurotsukiaroma-afk <kurotsukiaroma@gmail.com>
2026-06-01 16:37:47 -0500

    Update URL in Loadstring.lua for Loader script

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kurotsukiaroma-afk/Liquid-Admin/refs/heads/main/Liquid%20Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kurotsukiaroma-afk/Liquid-Admin/refs/heads/main/Loader.lua"))()
